697a7b823e0c321cc111ec1eb99406008ffd7973
v_ziaoliu <v_ziaoliu@tencent.com>
2020-06-24 19:15:36 +0800

    添加

---
+++ src/cart/ItemCard.tsx
@@ -0,0 +1,54 @@
+import React from "react"
+import { CartItem } from "./"
+import { OnCheckedChange } from './use-checked'
+import { Typography } from "antd"
+
+interface Props {
+  item: CartItem
+  checked: boolean
+  onCheckedChange: OnCheckedChange<CartItem>
+}
+
+// memo优化策略
+function areEqual(prevProps: Props, nextProps: Props) {
+  return (
+    prevProps.checked === nextProps.checked
+  )
+}
+
+const ItemCard = React.memo((props: Props) => {
+  console.log('cart item rerender')
+  const { item, checked, onCheckedChange } = props
+  const { name, price } = item
+
+  const checkBox = React.useRef<HTMLInputElement>(null);
+
+  // const onWrapCheckedChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   const { checked } = e.target
+  // }
+  
+  
+  return (
+    <div className="item-card" onClick={()=>{
+      if (checkBox && checkBox.current) {
+        const { checked } = checkBox.current;
+        onCheckedChange(item, !checked)
+      }
+    }}>
+      <div className="checkbox-wrap">
+        <input
+          ref={checkBox}
+          type="checkbox"
+          checked={checked}
+          // onChange={onWrapCheckedChange}
+        />
+      </div>
+      <p className="item-info">
+        {name} <Typography.Text >${price}</Typography.Text>
+      </p>
+    </div>
+  )
+}, areEqual)
+
+
+export default ItemCard
